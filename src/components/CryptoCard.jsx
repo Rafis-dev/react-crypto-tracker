ee69eee585a338f18ab087adccb131324bf6ba7c
Rafis Shigapov <rafis1work@gmail.com>
2026-01-27 17:56:21 +0300

    created formatter function with Intl api

--- src/components/CryptoCard.jsx
+++ src/components/CryptoCard.jsx
@@ -1,3 +1,22 @@
+import { formatPrice } from '../utils/formatter';
+
 export const CryptoCard = ({ crypto }) => {
-  return <div className="crypto-card"></div>;
+  return (
+    <div className="crypto-card">
+      <div className="crypto-header">
+        <div className="crypto-info">
+          <img src={crypto.image} alt={crypto.name} />
+          <div>
+            <h3>{crypto.name}</h3>
+            <p className="symbol">{crypto.symbol.toUpperCase()}</p>
+            <span className="rank">#{crypto.market_cap_rank}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="crypto-price">
+        <p className="price">{formatPrice(crypto.current_price)}</p>
+      </div>
+    </div>
+  );
 };
